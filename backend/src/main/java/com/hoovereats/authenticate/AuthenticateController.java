0f57149c2e2dd47143b9ece4b0b420c4205df417
anhtimelimitexceeded <anh.t.le22@gmail.com>
2021-10-09 01:51:40 -0400

    add java admin sdk

--- backend/src/main/java/com/hoovereats/authenticate/AuthenticateController.java
+++ backend/src/main/java/com/hoovereats/authenticate/AuthenticateController.java
@@ -1,22 +1,20 @@
-package com.hoovereats;
+package com.hoovereats.authenticate;
 
-// import org.springframework.web.bind.annotation.PostMapping;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
 public class AuthenticateController {
-  FirebaseOptions options = FirebaseOptions.builder()
-    .setCredentials(GoogleCredentials.getApplicationDefault())
-    .build();
-  FirebaseApp.initializeApp(options);
 	
   @PostMapping("/auth")
-	public String index(@RequestHeader Map<String, String> headers) {
-		System.out.println(headers);
-    FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-    String uid = decodedToken.getUid();  
-
+	public String index(@RequestHeader Map<String, String> headers) throws FirebaseAuthException {
+  	System.out.println(headers);
+  	System.out.println(headers.get("authorization"));
+    FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(headers.get("authorization"));
+    String uid = decodedToken.getUid();
     System.out.println(uid);
     return null;
 	}
--- backend/src/main/java/com/hoovereats/config/MyConfig.java
+++ backend/src/main/java/com/hoovereats/config/MyConfig.java
@@ -1,4 +1,4 @@
-package com.hoovereats;
+package com.hoovereats.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
